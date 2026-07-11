a5b86abd0f18a733019d6934408b1768b76722fb
Abdulkareem Attar <2004abdulkareem@gmail.com>
2026-07-11 06:10:53 +0300

    fix url

--- kids/urls.js
+++ kids/urls.js
@@ -8,7 +8,7 @@ router.post('/', validateAddingKid, addKid);
 
 router.get('/:id', validateGetKidsOf, getKidsOf);
 
-router.post('/:id/confirmKid', validateConfirmKid, confirmKid);
+router.post('/:id/confirm', validateConfirmKid, confirmKid);
 
 
 
