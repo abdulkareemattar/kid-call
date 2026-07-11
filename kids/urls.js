d2e22d8cfb0db5390f4864191551ff97bdfa8ef1
Abdulkareem Attar <2004abdulkareem@gmail.com>
2026-07-11 13:33:34 +0300

    feat: expose admin endpoint to list all kids

--- kids/urls.js
+++ kids/urls.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { addKid, getKidsOf } from './kids.js';
+import { addKid, getKidsOf,getAllKids } from './kids.js';
 import { validateAddingKid, validateGetKidsOf } from './validators.js';
-
 export const router = express.Router();
 
 router.post('/', validateAddingKid, addKid);
-
+router.get('/admin/all', getAllKids);
 router.get('/:id', validateGetKidsOf, getKidsOf);
 
