f548ccfd63ebf288210042b7741664af20c50b95
Abdulkareem Attar <2004abdulkareem@gmail.com>
2026-07-14 15:40:24 +0300

    fix merge conflict and update routes

--- kids/urls.js
+++ kids/urls.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { addKid, getKidsOf } from './kids.js';
-import { validateAddingKid, validateGetKidsOf } from './validators.js';
+import { addKid, getKidsOf, confirmKid,callKid,getAllKids } from './kids.js';
+import { validateAddingKid, validateGetKidsOf, validateConfirmKid,validateCallKid } from './validators.js';
 
 export const router = express.Router();
 
 router.post('/', validateAddingKid, addKid);
 router.get('/admin/all', getAllKids);
-router.get('/:id', validateGetKidsOf, getKidsOf);
 router.post('/:id/call', validateCallKid, callKid);
 
 router.patch('/:id/confirm', validateConfirmKid, confirmKid);
+router.get('/:id', validateGetKidsOf, getKidsOf);
